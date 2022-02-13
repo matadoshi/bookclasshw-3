a70869a85419e7c7fdccda548cfe5e9ff539ff3b
onursmlv <onurtis@code.edu.az>
2022-02-13 17:52:01 +0400

    Done Task/3

---
+++ Book.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Task3Book
+{
+//Task - 3
+//Asagidaki memberlerden ibaret Book class-i yaradirsiniz: 
+//Name
+//PageCount
+//ps: name ve count deyerleri teyin olunmammis Book obyekti yaranmamalidir
+//PageCount deyeri 10-dan kicik ola bilmez ve Name deyerinin uzunlugu 3-den qisa ola bilmez
+//Main hissesinde 10 kitabliq Book arrayi olsun.
+//Kitab deyerleri console-dan daxil edilsin ve yaradilsin, array-e elave edilsin.Sonda arraydeki kitablarin
+//Name ve PageCount deyerleri console penceresinde gosterilsin
+
+    class Book
+    {
+        public string Name;
+        public int PageCount;
+
+        public Book(string name,int pagecount)
+        {
+            
+            while (true)
+            {
+                
+                if (name.Length > 3)
+                {
+                    Name = name;
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Adini Yeniden Daxil Edin");
+                    name = Console.ReadLine();
+                }
+               
+            }
+            while (true)
+            {
+                if (pagecount > 10)
+                {
+
+                    PageCount= pagecount;
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Sayini Duzdun Daxil Edin");
+                    int.TryParse(Console.ReadLine(), out pagecount);
+                }
+            }
+           
+
+        }
+        public string Info()
+        {
+            return $"{Name} {PageCount}";
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Task3Book
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Book[] str =new Book[10];
+            for (int i = 0; i < str.Length; i++)
+            {
+                Console.WriteLine($"{i+1}.Kitabin adini daxil edin :");
+                string temp_name = Console.ReadLine();
+                Console.WriteLine($"{i+1} Kitabin sehfesinin sayini daxil edin");
+                int.TryParse(Console.ReadLine(), out int a);
+                Book test = new Book(temp_name, a);
+                str[i] = test;
+            }
+            foreach (var item in str)
+            {
+                Console.WriteLine(item.Info());
+            }
+        }
+
+    }
+}
